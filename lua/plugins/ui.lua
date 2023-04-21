087368820d0e93d286102ce0d6397b42293c6bb2
Tomáš Tobrman <30987354+TomasTobrman@users.noreply.github.com>
2023-04-21 08:26:00 +0200

    Updated UI

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -7,8 +7,8 @@ return {
 			options = {
 				globalstatus = false,
 				icons_enabled = true,
+				component_separators = '|',
 				section_separators = { left = '', right = '' },
-				component_separators = { left = '', right = '' },
 				disabled_filetypes = {
 				},
 				ignore_focus = {
@@ -16,21 +16,23 @@ return {
 				},
 			},
 			sections = {
-				lualine_a = {'mode'},
-				lualine_b = {'branch', 'diff', 'diagnostics'},
-				lualine_c = {'filename'},
-				lualine_x = {'encoding', 'filetype'},
-				lualine_y = {'progress'},
-				lualine_z = {'location'},
+				lualine_a = {{ 'mode', separator = { left = '' }, right_padding = 2 }},
+				lualine_b = {'filename', 'branch', 'diff'},
+				lualine_c = {'diagnostics'},
+				lualine_x = {},
+				lualine_y = {'filetype', 'progress'},
+				lualine_z = {{ 'location', separator = { right = '' }, left_padding = 2 }},
 			},
 			inactive_sections = {
-				lualine_a = {},
+				lualine_a = {'filename'},
 				lualine_b = {},
-				lualine_c = {'filename'},
+				lualine_c = {},
 				lualine_x = {},
 				lualine_y = {},
-				lualine_z = {},
+				lualine_z = {'location'},
 			},
+			tabline = {},
+			extensions = {},
 		},
 	},
 	{
